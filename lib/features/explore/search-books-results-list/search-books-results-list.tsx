b6edfddbb54ee3a9256f30f346c3e1100a17566a
Al Miller <support@urbanform.us>
2025-02-08 19:57:08 -0800

    wip

--- lib/features/explore/search-books-results-list/search-books-results-list.tsx
+++ lib/features/explore/search-books-results-list/search-books-results-list.tsx
@@ -1,37 +1,3 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import {
-  getBookContentById,
-  getBookMetadataById,
-} from "../client/gutenberg-client";
-
-const schema = z.object({
-  bookId: z.string().length(10, {
-    message: "Gutenberg book ID's are 10 characters long.",
-  }),
-});
-type Schema = z.infer<typeof schema>;
-
 export function SearchBooksResultsList() {
-
-  return (
-   < 
-  )
+  return <div>SearchBooksResultsList</div>;
 }
