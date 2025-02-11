5bf88c0b4c26bf7dee39948a7357e8871d8f710d
Al Miller <al.norman.miller@gmail.com>
2025-02-10 23:37:25 -0800

    adjust saved book

---
+++ prisma/migrations/20250211073713_adjust_saved_book/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `gutenbergBookImageHref` on the `saved_book` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "saved_book" DROP COLUMN "gutenbergBookImageHref";
