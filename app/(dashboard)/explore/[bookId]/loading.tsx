dfd7cca979aecda3d5f5b131032bc713ed90074e
Albert Miller <al.norman.miller@gmail.com>
2025-02-12 23:39:17 -0800

    fix: just increase single path, ui fixes (#14)
    * small fixes
    
    * update path
    
    * increase batch
    
    * 10000
    
    * bump

--- app/(dashboard)/explore/[bookId]/loading.tsx
+++ app/(dashboard)/explore/[bookId]/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <Skeleton className="h-28 p-4 flex flex-col justify-between w-full xl:w-4/5 mx-auto text-muted-foreground">
-      Reading and analyzing books, this will take a minute...
+      Reading and analyzing, this will take a minute...
     </Skeleton>
   );
 }
--- lib/features/chat/embeddings.ts
+++ lib/features/chat/embeddings.ts
@@ -4,7 +4,7 @@ import { generateEmbedding, generateEmbeddings } from "@/lib/adapters/openai";
 import { prisma } from "@/prisma/client";
 import { Prisma } from "@prisma/client";
 
-const BATCH_SIZE = 500;
+const BATCH_SIZE = 13000;
 
 export const writeEmbeddings = async ({
   gutenbergBookId,
@@ -20,9 +20,9 @@ export const writeEmbeddings = async ({
     where: { gutenbergBookId },
   });
 
-  for (let i = 0; i < embeddings.length; i += BATCH_SIZE) {
-    const batch = embeddings.slice(i, i + BATCH_SIZE);
-    try {
+  try {
+    for (let i = 0; i < embeddings.length; i += BATCH_SIZE) {
+      const batch = embeddings.slice(i, i + BATCH_SIZE);
       const rows = Prisma.join(
         batch.map(
           (embedding) =>
@@ -33,9 +33,9 @@ export const writeEmbeddings = async ({
       );
 
       await prisma.$executeRaw`INSERT INTO embedding ("gutenbergBookMetadataId", vector, content) VALUES ${rows}`;
-    } catch (e) {
-      console.error(e);
     }
+  } catch (e) {
+    console.error(e);
   }
 };
 
